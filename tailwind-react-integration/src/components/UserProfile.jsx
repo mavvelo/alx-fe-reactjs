507c47c5beefb59c4d331d58508ef00d4c1c5473
Isaac Cobby Andoh <cobbjy@gmail.com>
2024-10-02 10:28:07 -0400

    updates

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 function UserProfile() {
   return (
     <div className="user-profile bg-gray-100 md:p-8 sm:p-4 md:max-w-sm max-w-xs mx-auto my-20 rounded-lg shadow-lg">
-      <img src="https://via.placeholder.com/150" alt="User" className="rounded-full w-24 h-24 sm:w-36 sm:h-36 mx-auto" />
+      <img src="https://via.placeholder.com/150" alt="User" className="rounded-full sm:w-24 sm:h-24 md:w-36 md:h-36 mx-auto" />
       <h1 className="text-lg md:text-xl text-blue-800 my-4">John Doe</h1>
       <p className="text-sm sm:text-base text-gray-600">Developer at Example Co. Loves to write code and explore new technologies.</p>
     </div>
