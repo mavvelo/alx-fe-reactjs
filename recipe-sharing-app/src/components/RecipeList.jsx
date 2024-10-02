e0ec6f93a8dffc742414c02b4ca9da4e61134ab4
Isaac Cobby Andoh <cobbjy@gmail.com>
2024-10-02 06:18:19 -0400

    recipe app

--- recipe-sharing-app/src/components/RecipeList.jsx
+++ recipe-sharing-app/src/components/RecipeList.jsx
@@ -3,7 +3,21 @@ import { useRecipeStore } from './recipeStore';
 import { Link } from 'react-router-dom'; // Import Link for routing
 
 const RecipeList = () => {
-  const recipes = useRecipeStore(state => state.filteredRecipes); // Use filteredRecipes
+  const { filteredRecipes } = useRecipeStore(); // Use filteredRecipes
 
-  // ... rest of your code to render the list of recipes using recipes array
+  return (
+    <div>
+      <h2>Recipes</h2>
+      {filteredRecipes.map(recipe => ( // Map over filteredRecipes
+        <div key={recipe.id}>
+          <h3>{recipe.title}</h3>
+          <p>{recipe.description}</p>
+          {/* Add other recipe details as needed */}
+          <Link to={`/recipe/${recipe.id}`}>View Recipe</Link>
+        </div>
+      ))}
+    </div>
+  );
 };
+
+export default RecipeList;
