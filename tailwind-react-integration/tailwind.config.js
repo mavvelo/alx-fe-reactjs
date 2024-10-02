029ae82266997a0809cad009a233074c4a1f508a
Isaac Cobby Andoh <cobbjy@gmail.com>
2024-10-02 10:22:07 -0400

    updates

--- tailwind-react-integration/tailwind.config.js
+++ tailwind-react-integration/tailwind.config.js
@@ -1,4 +1,11 @@
 module.exports = {
   purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
-  // ... other configuration options
+  darkMode: false, // Enable dark mode if desired
+  theme: {
+    extend: {} // Customize the default theme here
+  },
+  variants: {
+    extend: {} // Customize variant behavior here
+  },
+  plugins: [] // Add custom plugins here
 };
