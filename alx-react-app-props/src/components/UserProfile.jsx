97fc28aa9add0efd42657d5500436c8132598c99
Isaac Cobby Andoh <cobbjy@gmail.com>
2024-10-02 04:57:33 -0400

    updates

--- alx-react-app-props/src/components/UserProfile.jsx
+++ alx-react-app-props/src/components/UserProfile.jsx
@@ -1,9 +1,14 @@
-function UserProfile(props) {
+import React, { useContext } from 'react';
+import UserContext from './UserContext';
+
+function UserProfile() {
+  const { userData } = useContext(UserContext);
+
   return (
-    <div className="user-profile">
-      <h2>{props.name}</h2>
-      <p>Age: {props.age}</p>
-      <p>Bio: {props.bio}</p>
+    <div>
+      <h2>{userData.name}</h2>
+      <p>Age: {userData.age}</p>
+      <p>Bio: {userData.bio}</p>
     </div>
   );
 }
