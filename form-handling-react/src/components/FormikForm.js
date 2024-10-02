86af6fd8bb607e7d2f8286de48a9bdd4eb7ac7a0
Isaac Cobby Andoh <cobbjy@gmail.com>
2024-10-02 06:37:02 -0400

    updates

--- form-handling-react/src/components/FormikForm.js
+++ form-handling-react/src/components/FormikForm.js
@@ -1,44 +1,62 @@
+// formikForm.js
 import React from 'react';
 import { Formik, Form, Field, ErrorMessage } from 'formik';
 import * as Yup from 'yup';
 
-const validationSchema = Yup.object().shape({
-  username: Yup.string().required('Username is required'),
-  email: Yup.string().email('Invalid email').required('Email is required'),
-  password: Yup.string().required('Password is required'),
+const SignupSchema = Yup.object().shape({
+  firstName: Yup.string()
+    .min(2, 'Too Short!')
+    .max(50, 'Too Long!')
+    .required('Required'),
+  lastName: Yup.string()
+    .min(2, 'Too Short!')
+    .max(50, 'Too Long!')
+    .required('Required'),
+  email: Yup.string()
+    .email('Invalid email')
+    .required('Required'),
+  // Add more validation rules as needed
 });
 
 function FormikForm() {
   return (
-    <Formik
-      initialValues={{ username: '', email: '', password: '' }}
-      validationSchema={validationSchema}
-      onSubmit={(values) => {
-        // Handle form submission here, e.g., send data to API
-        console.log('Form submitted:', values);
-      }}
-    >
-      {({ errors, touched }) => (
-        <Form>
-          <div>
-            <label htmlFor="username">Username:</label>
-            <Field type="text" name="username" />
-            <ErrorMessage name="username" component="div" />
-          </div>
-          <div>
-            <label htmlFor="email">Email:</label>
-            <Field type="email" name="email" />
-            <ErrorMessage name="email" component="div" />
-          </div>
-          <div>
-            <label htmlFor="password">Password:</label>
-            <Field type="password" name="password" />
-            <ErrorMessage name="password" component="div" />
-          </div>
-          <button type="submit">Submit</button>
-        </Form>
-      )}
-    </Formik>
+    <div>
+      <h1>Signup</h1>
+      <Formik
+        initialValues={{
+          firstName: '',
+          lastName: '',
+          email: '',
+        }}
+        validationSchema={SignupSchema}
+        onSubmit={(values) => {
+          // Handle form submission (e.g., send data to the server)
+          console.log('Form submitted:', values);
+        }}
+      >
+        {({ errors, touched }) => (
+          <Form>
+            <div>
+              <label htmlFor="firstName">First Name:</label>
+              <Field type="text" id="firstName" name="firstName" />
+              <ErrorMessage name="firstName" component="div" />
+            </div>
+            <div>
+              <label htmlFor="lastName">Last Name:</label>
+              <Field type="text" id="lastName" name="lastName" />
+              <ErrorMessage name="lastName" component="div" />
+            </div>
+            <div>
+              <label htmlFor="email">Email:</label>
+              <Field type="email" id="email" name="email" />
+              <ErrorMessage name="email" component="div" />
+            </div>
+            {/* Add more fields here */}
+            <button type="submit">Register</button>
+          </Form>
+        )}
+      </Formik>
+    </div>
   );
 }
 
