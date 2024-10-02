fc8252934ce36d8c71665ddaff07ebc20fb2104a
Isaac Cobby Andoh <cobbjy@gmail.com>
2024-10-02 04:26:07 -0400

    updates

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -3,6 +3,7 @@ import Header from './components/Header';
 import MainContent from './components/MainContent';
 import Footer from './components/Footer';
 import UserProfile from './components/UserProfile';
+import Counter from './components/Counter';
 
 function App() {
   const user = {
@@ -16,6 +17,7 @@ function App() {
           <Header />
           <UserProfile {...user} />
           <MainContent />
+          <Counter />
           <Footer />
       </div>
   );
